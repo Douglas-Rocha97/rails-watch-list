c3df729a5949c95bfc1a8e12b88c40121a0fa0a0
douglas rocha <douglashidekazu15@gmail.com>
2025-08-15 13:46:27 +0900

    root to index

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find((params[:list_id]))
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      redirect_to new_list_bookmark_path, status: :unprocessable_entity
+    end
+
+    def destroy
+      raise
+    end
+
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
